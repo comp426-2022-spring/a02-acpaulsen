886fc86dae42866c2b3b597a068435d629b98f23
acpaulsen <apaulsen43@gmail.com>
2022-04-26 19:56:22 -0400

    minimist fix attemp

--- guess-flip.js
+++ guess-flip.js
@@ -3,9 +3,9 @@ import {flipACoin} from "./modules/coin.mjs";
 const require = createRequire(import.meta.url);
 const args = (require("./node_modules/minimist"))(process.argv.slice(2));
 
-var gloog = minimist(process.argv.slice(2))
+var minimist = minimist(process.argv.slice(2))
 
-var froog = gloog.call;
+var froog = args.call;
 if ((froog == "heads") | (froog == "tails")) {
     console.log(flipACoin(froog))
 }
